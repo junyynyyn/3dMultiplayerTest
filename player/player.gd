661e0d648516c17af7b8c5627ec5f6744eb1daef
Jun <yeojy@student.unimelb.edu.au>
2024-12-26 12:49:53 +1100

    Add basic ammo and health system

--- player/player.gd
+++ player/player.gd
@@ -4,12 +4,47 @@ extends CharacterBody3D
 @export var JUMP_VELOCITY = 4.5
 @export var SENSITIVITY = 0.01
 
+@export var max_health = 10
+var health: int = max_health:
+	set(value):
+		health = value
+		health_bar_ui.value = value
+
+@export var damage = 2
+@export var fire_rate = 2.0
+@export var reload_speed = 1.0
+@export var max_ammo = 10
+var ammo: int = max_ammo:
+	set(value):
+		ammo = value
+		ammo_ui.text = str(ammo) + "/" + str(max_ammo)
+
 @onready var camera_holder: Node3D = $CameraHolder
 @onready var player_camera: Camera3D = $CameraHolder/PlayerCamera
+@onready var ray_cast_3d: RayCast3D = $CameraHolder/PlayerCamera/RayCast3D
+
+@onready var fire_rate_timer: Timer = $FireRateTimer
+@onready var reload_timer: Timer = $ReloadTimer
+
+@onready var ammo_ui: Label = $UI/UIFrame/AmmoUI
+@onready var health_bar_ui: ProgressBar = $UI/UIFrame/HealthBarUI
 
 func _ready() -> void:
 	# Confine Mouse to the Window
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	# Set 
+	fire_rate_timer.wait_time = 1 / fire_rate
+	reload_timer.wait_time = reload_speed
+	
+func _process(delta: float) -> void:
+	if (fire_rate_timer.is_stopped() and reload_timer.is_stopped()):
+		if (Input.is_action_just_pressed("fire")):
+			fire()
+			ammo -= 1
+			fire_rate_timer.start()
+			if (ammo <= 0):
+				reload_timer.start()
 
 func _physics_process(delta: float) -> void:
 	# Basic Movement Code
@@ -36,3 +71,9 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera_holder.rotate_y(-event.relative.x * SENSITIVITY)
 		player_camera.rotate_x(-event.relative.y * SENSITIVITY)
 		player_camera.rotation.x = clamp(player_camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func fire() -> void:
+	print(ray_cast_3d.get_collision_point())
+
+func _on_reload_timer_timeout() -> void:
+	ammo = max_ammo
